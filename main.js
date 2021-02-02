2de0f0cb377925acdf4d77c055d9575a585f5bd8
Just_Duck <67290167+lizakovalsky@users.noreply.github.com>
2021-02-02 20:48:44 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function () {
+
+    let butPage = document.getElementById("button_page");
+    let butStyle = document.getElementById("button_style");
+    butPage.addEventListener('click', function (){
+      window.location.href = "https://www.youtube.com/watch?v=ZpAQotqUSZ8";
+    });
+    butStyle.addEventListener('click', function(){
+      document.body.style.backgroundColor = "lime";
+    });
+});
